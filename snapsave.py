dab9f70dcc7569fc5a6bc8d7d7f74f27d7ffaabd
Rob Brown <robertb@sec-1.com>
2017-06-28 18:29:54 +0100

    Progress.

--- snapsave.py
+++ snapsave.py
@@ -10,16 +10,33 @@ accessTokenFile.close()
 dbx = dropbox.Dropbox(accessToken)
 
 parser = OptionParser()
-parser.add_option("-f", "--sourcefolder", dest="sourcefolder",
-                  help="Folder to save", metavar="folder")
+parser.add_option("-s", "--sourcefolder", dest="sourcefolder",
+                    help="Folder to save", metavar="folder")
+parser.add_option("-d", "--destinationfolder", dest="destinationfolder",
+                    help="Destination on Dropbox",metavar="dropbox_folder")
+parser.add_option("-v", action="store_true", dest="verbose", help="Output debugging information")
 
 (options, args) = parser.parse_args()
 
-if options.sourcefolder is None:
+if options.sourcefolder is None or options.destinationfolder is None:
     parser.print_help()
     exit()
 
 if os.path.isdir(options.sourcefolder):
-    pass
+    # Normalise this folder 
+    destinationFolderName = "/" + options.destinationfolder
+    
+    # Delete remote folder
+    if options.verbose:
+        print "About to delete remote folder \"" + destinationFolderName  + "\""
+    
+    dbx.files_delete(destinationFolderName)
+
+    # Create remote folder
+    if options.verbose:
+        print "About to create remote folder \"" + destinationFolderName  + "\""
+
+    #dbx.files_create_folder(destinationFolderName)
+    # Upload files
 else:
     print "Path \"" + options.sourcefolder + "\" not found."
