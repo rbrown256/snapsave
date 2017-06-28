6b2539fcfadd5c5d49682ce18150a8b9ce31304a
Rob Brown <robertb@sec-1.com>
2017-06-28 10:22:37 +0100

    Command line options

--- snapsave.py
+++ snapsave.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python
 import dropbox
+from optparse import OptionParser
 
 accessTokenFile = open("access_token.tkn", "r")
-
-
+accessToken = accessTokenFile.read()
+accessTokenFile.close()
 
 dbx = dropbox.Dropbox(accessToken)
 
+parser = OptionParser()
+parser.add_option("-f", "--sourcefolder", dest="sourcefolder",
+                  help="Folder to save", metavar="folder")
+
+(options, args) = parser.parse_args()
+
+print accessToken
